7732e55abff3c68897a4af81f3f60ebd0767f63c
csanjeewag <csanjeewag@gmail.com>
2020-05-26 16:58:47 +0530

    remove style link v2

--- layouts/MainLayout2.js
+++ layouts/MainLayout2.js
@@ -79,7 +79,7 @@ class Layout extends Component {
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 
                   
-                  {<link rel="stylesheet" type="text/css" href="./../fonts/font-awesome-4.7.0/css/font-awesome.min.css"/>}
+                  {/*<link rel="stylesheet" type="text/css" href="./../fonts/font-awesome-4.7.0/css/font-awesome.min.css"/>*/}
                   {<link rel="stylesheet" type="text/css" href="./../fonts/iconic/css/material-design-iconic-font.min.css"/>}
                   {/*<link rel="stylesheet" type="text/css" href="./../fonts/linearicons-v1.0.0/icon-font.min.css"/>*/}
                   {/*<link rel="stylesheet" type="text/css" href="./../vendor/animate/animate.css"/>*/}
